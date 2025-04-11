188ffe55ac6536b7789aaef43539dbd3413584f5
yvr8 <valleviieu@gmail.com>
2025-04-11 16:37:41 -0400

    personnage v1

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -1,33 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-    private PlayerInputReader inputReader;
-    private Vector2 direction;
-    // Start is called before the first frame update
+    // Composant principale du joueur
+    private PlayerInputReader _inputReader;
+    private Vector2 _direction;
+    private Rigidbody2D _rigidbody;
+    // Variable modifiable pour changer le comportement du personnage
+    public float MagnitudeSaut;
+    public float MagnitudeDeplacement;
+    public float VitesseDeplacementMax;
+    // Variable utilise dans le script
+    private 
     void Start()
     {
-        inputReader = GetComponent<PlayerInputReader>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _inputReader = GetComponent<PlayerInputReader>();
         // S'abonner à l'événement sauter
-        inputReader.BS.callback += Sauter;
-        inputReader.LS_m.callback += Deplacer;
+        _inputReader.BS.callback += Sauter;
+        _inputReader.LS_m.callback += Deplacer;
+        
+        _direction = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * Time.deltaTime);
+        _rigidbody.velocity = new Vector2(Mathf.Clamp(_rigidbody.velocity.x + _direction.x * MagnitudeDeplacement, -VitesseDeplacementMax, VitesseDeplacementMax), _rigidbody.velocity.y);
     }
 
     void Sauter()
     {
-        transform.Translate(Vector2.up * 3);
+        _rigidbody.velocity = Vector2.up * MagnitudeSaut;
     }
     void Deplacer(Vector2 newDirection)
     {
-        direction = newDirection;
-        Debug.Log(direction);
+        _direction.x = newDirection.x;
     }
 }
