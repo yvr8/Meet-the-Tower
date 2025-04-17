34a78e81edf1a8690e21086e7b98bdfa2048952d
yvr8 <valleviieu@gmail.com>
2025-04-17 19:17:42 -0400

    mouvement brise

--- Assets/Script/Mouvement.cs
+++ Assets/Script/Mouvement.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Mouvement : MonoBehaviour
@@ -9,7 +5,8 @@ public class Mouvement : MonoBehaviour
     // Composant principale du joueur
     private PlayerInputReader _inputReader;
     private Rigidbody2D _rigidbody;
-    public GameObject _camera;
+    private Collider2D _collider2D;
+    public GameObject camera;
     // Variable modifiable pour changer le comportement du personnage
     public float MagnitudeSaut;
     public float VitesseDeplacement;
@@ -19,34 +16,43 @@ public class Mouvement : MonoBehaviour
     void Start()
     {
         // Definir les composants de base
+        _collider2D = GetComponent<Collider2D>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _inputReader = GetComponent<PlayerInputReader>();
-        _camera = GameObject.FindWithTag("MainCamera");
+        camera = GameObject.FindWithTag("MainCamera");
+        
         // S'abonner à l'événement sauter
         _inputReader.BS.callback += Sauter;
         _inputReader.LS_m.callback += Deplacer;
+        
         // definir les vecteur a zero
         _directionDeplacement = Vector2.zero;
-        _vecteurVelocite = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Deplacer camera
-        _camera.transform.position = new Vector2(_camera.transform.position.x, transform.position.y);
+        camera.transform.position = new Vector2(camera.transform.position.x, transform.position.y);
         // Deplacement  
-        _vecteurVelocite = _rigidbody.velocity;
-        _rigidbody.velocity = new Vector2(Mathf.Clamp(_directionDeplacement.x * VitesseDeplacement, -VitesseDeplacement, VitesseDeplacement), _vecteurVelocite.y) ;
-    }
+        _rigidbody.velocity = new Vector2(_directionDeplacement.x, _rigidbody.velocity.y );
+    }   
 
     void Sauter()
     {
-        _rigidbody.AddForce(Vector2.up * MagnitudeSaut);
+        Debug.Log("Sauter");
+        RaycastHit2D[] hit = new RaycastHit2D[0];
+        Debug.Log(_collider2D.Cast(Vector2.down, hit , 5f));
+        if (_collider2D.Cast(Vector2.down, hit , 5f) > 0)
+        {
+            Debug.Log(_collider2D.Cast(Vector2.down, hit , 0.01f));
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, MagnitudeSaut);
+        }
     }
+    
     // Update quand un changement ce fait dans la valeur LS_m
     void Deplacer(Vector2 newDirection)
     {
-        _directionDeplacement.x = newDirection.x;
+        _directionDeplacement.x = newDirection.x * VitesseDeplacement;
     }
 }
