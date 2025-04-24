69942019ddffd12706cf7923602404d1fa105829
Adampro47 <Adambed6@outlook.com>
2025-04-24 17:26:37 -0400

    Ajout prefab Etoile, chronomanger, etoilemanager,porte

--- Assets/Script/FinirNiveau.cs
+++ Assets/Script/FinirNiveau.cs
@@ -1,39 +1,71 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-// Ce code est le test lié au bouton, il est amené a disparaitre (la porte vas faire sa)
+
 public class FinNiveauBouton : MonoBehaviour
 {
-    public bool toutesPiecesRecuperees = true;
-    public float tempsUtilise = 75.312122f;
-    public Button btnRetourMenu;      
+    public Button btnRetourMenu;
+    private ChronoManager chronoManager;  // Référence au ChronoManager
+    public string tagEtoile = "Etoile";      // Tag utilisé pour les étoiles
 
     void Start()
     {
-  
+        // Recherche du ChronoManager dans la scène
+        chronoManager = FindObjectOfType<ChronoManager>();
+
         if (btnRetourMenu != null)
         {
             btnRetourMenu.onClick.AddListener(FinirNiveau);
         }
-
     }
 
     void FinirNiveau()
     {
         Debug.Log("FinirNiveau");
+
+        float tempsActuel = chronoManager.GetTime();
+        bool toutesEtoilesRecupereesMaintenant = VerifierToutesLesEtoilesRecuperees();
         string nomNiveau = SceneManager.GetActiveScene().name;
+
+        DonneesNiveau donneesExistantes = SystemeSauvegarde.Instance.GetNiveau(nomNiveau);
+
+        bool garderEtoiles = toutesEtoilesRecupereesMaintenant;
+        float tempsFinal = tempsActuel;
+
+        if (donneesExistantes != null)
+        {
+            // Garder les étoiles si elles ont été récupérées avant
+            if (donneesExistantes.toutesPiecesRecuperees)
+                garderEtoiles = true;
+
+            // Garder le meilleur temps (le plus petit)
+            if (donneesExistantes.tempsUtilise < tempsActuel)
+                tempsFinal = donneesExistantes.tempsUtilise;
+        }
 
         SystemeSauvegarde.Instance.MettreAJourNiveau(
             nomNiveau,
             true,
-            toutesPiecesRecuperees,
-            tempsUtilise
+            garderEtoiles,
+            tempsFinal
         );
+
+        // Retourner au menu
         RetourAcceuil();
+    }
+
+    bool VerifierToutesLesEtoilesRecuperees()
+    {
+        // Rechercher tous les objets avec le tag "Star"
+        GameObject[] etoiles = GameObject.FindGameObjectsWithTag(tagEtoile);
+
+        // Si le nombre d'étoiles dans la scène est égal à 0, alors toutes les étoiles sont récupérées
+        return etoiles.Length == 0;
     }
 
     void RetourAcceuil()
     {
+        // Charger la scène d'accueil
         SceneManager.LoadScene(sceneName: "Acceuil");
     }
 }
