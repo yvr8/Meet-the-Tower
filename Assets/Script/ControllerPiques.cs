752dc894ef1889560229467f28ba71c04573751e
yvr8 <146970761+yvr8@users.noreply.github.com>
2025-04-24 18:18:49 -0400

    mode split et side fonctionnel pour pique dynamique

--- Assets/Script/ControllerPiques.cs
+++ Assets/Script/ControllerPiques.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
@@ -11,6 +13,7 @@ public class ControllerPiques : MonoBehaviour
     public PiqueDynamique[] listePique;
     public float waitBeforeStart = 2f;
     public float repeatRate = 10f;
+    public float delayVague = 0.5f;
     public enumStatePique[] statePique;
 
     //index pour la suite d'animation fait par les piques
@@ -57,16 +60,16 @@ public class ControllerPiques : MonoBehaviour
                 StartCoroutine(Vague());
                 break;
             case enumStatePique.Split11:
-                Split(1, 1);
+                Split(1, 1, false);
                 break;
             case enumStatePique.Split22:
-                Split(2, 2);
+                Split(2, 2, false);
                 break;
             case enumStatePique.Split33:
-                Split(3, 3);
+                Split(3, 3, false);
                 break;
             case enumStatePique.Split44:
-                Split(4, 4);
+                Split(4, 4, false);
                 break;
         }
         index++;
@@ -82,21 +85,19 @@ public class ControllerPiques : MonoBehaviour
 
     private void Side(bool reverse)
     {
-        int centre = listePique.Length - 1;
-        // gauche -> droite
-        if (!reverse)
+        int centre = listePique.Length / 2;
+
+        for (int i = 0; i < listePique.Length; i++)
         {
-            for (int i = 0; i <= centre; i++)
+            if (i >= centre)
             {
+                if (!reverse)
                 listePique[i].Activate();
             }
-        }
-        // droit -> gauche
-        else
-        {
-            for (int i = 0; i <= centre; i--)
+            else
             {
-                listePique[i].Activate();
+                if (reverse)
+                    listePique[i].Activate();
             }
         }
     }
@@ -105,7 +106,7 @@ public class ControllerPiques : MonoBehaviour
         foreach (PiqueDynamique pique in listePique)
         {
             pique.Activate();
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(delayVague);
         }
     }
 
@@ -116,13 +117,23 @@ public class ControllerPiques : MonoBehaviour
     /// <param name="groupeSize2">Nombre de pique dans le deuxieme regroupement</param>
     /// <param name="reverse">Inverse le groupe des piques qui sont active</param>
     /// <returns></returns>
-    private void Split(int groupeSize1, int groupeSize2)
+    private void Split(int groupSize1, int groupSize2, bool reverse)
     {
+        int longeurCycle = groupSize1 + groupSize2;
+
         for (int i = 0; i < listePique.Length; i++)
         {
-            if (groupeSize1 / (groupeSize1 + groupeSize2) <= i % (groupeSize1 + groupeSize2))
+            int positionCycle = i % longeurCycle;
+
+            if (positionCycle >= groupSize1)
             {
-                listePique[i].Activate();
+                if (!reverse)
+                    listePique[i].Activate();
+            }
+            else if(positionCycle < groupSize1)
+            {
+                if(reverse)
+                    listePique[i].Activate();
             }
         }
     }
