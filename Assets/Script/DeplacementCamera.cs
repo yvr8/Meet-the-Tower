107796d88441f177f5e523d88a04348221a4db95
yvr8 <146970761+yvr8@users.noreply.github.com>
2025-04-24 16:27:49 -0400

    Dezoom de la camera pour garder les joueurs dans le focus

--- Assets/Script/DeplacementCamera.cs
+++ Assets/Script/DeplacementCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,30 +8,39 @@ public class DeplacementCamera : MonoBehaviour
 {
     public float ammortissement;
     private Vector2 _vector0;
+    private Camera _camera;
     private GameObject[] _joueurs;
     // Start is called before the first frame update
     void Start()
     {
         _joueurs = GameObject.FindGameObjectsWithTag("Player");
+        _camera = GetComponent<Camera>();
         _vector0 = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DeplacerCamera();
+        UpdateCamera();
     }
 
-    void DeplacerCamera()
-    {      Vector3 pos = new Vector2();
-             foreach (GameObject joueur in _joueurs)
-             {
-                 pos += joueur.transform.position;
-             }
-             pos /= _joueurs.Length;
-             
-             transform.position = Vector2.SmoothDamp(transform.position, pos, ref _vector0, ammortissement );
+    void UpdateCamera()
+    {      
+        // Deplace la camera sur la moyenne de la position des joueurs
+        Vector3 pos = new Vector2();
+         foreach (GameObject joueur in _joueurs)
+         {
+             pos += joueur.transform.position;
          }
-     }
+         pos /= _joueurs.Length;
+         
+         transform.position = Vector2.SmoothDamp(transform.position, pos, ref _vector0, ammortissement );
+         
+         // Zoom la camera pour y inclure les deux joueurs
+         _camera.orthographicSize = Mathf.Max(10f, Vector2.Distance(_joueurs[0].transform.position, _joueurs[1].transform.position) * 0.5f);
+    }
+
+}
+
 
   
