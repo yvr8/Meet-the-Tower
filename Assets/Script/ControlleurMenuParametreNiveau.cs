05d0521ebb7b92eb1a1fd953d2da29afb88a3fe2
Adam <Adambed6@outlook.com>
2025-05-08 16:43:08 -0400

    Le Menu pour de pause fonctionne dans le niveau

---
+++ Assets/Script/ControlleurMenuParametreNiveau.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ControlleurMenuParametreNiveau : MonoBehaviour
+{
+    public Button btnRetourParametre;
+    public Button btnQuitter;
+    public GameObject MenuParametre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnRetourParametre.onClick.AddListener(btnRetourParametre_onClick);
+        btnQuitter.onClick.AddListener(btnQuitter_onClick);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void btnQuitter_onClick()
+    {
+        SceneManager.LoadScene("Acceuil");
+    }
+    void btnRetourParametre_onClick()
+    {
+        MenuParametre.SetActive(false);
+    }
+}
